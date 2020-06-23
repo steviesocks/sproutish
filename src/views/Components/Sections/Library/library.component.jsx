ac1c50e5b02851603508de55c07df4ee3a220770
steviesocks <stephen.stocking@gmail.com>
2020-06-23 14:14:52 -0400

    connected landing page to server and database

--- src/views/Components/Sections/Library/library.component.jsx
+++ src/views/Components/Sections/Library/library.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // react components for routing our app without refresh
 // import { Link } from "react-router-dom";
 // @material-ui/core components
@@ -10,21 +10,38 @@ import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import LibraryCard from "components/LibraryCard/LibraryCard.component";
 
-
 import styles from "assets/jss/material-kit-react/views/componentsSections/exampleStyle.js";
 
 import database from "database/database";
-
 
 const useStyles = makeStyles(styles);
 
 export default function Library() {
   const classes = useStyles();
+
+  const [ bookList, setBookList ] = useState([]);
+  const [libraryFilter, setLibraryFilter] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect triggered');
+    fetch('http://localhost:3000/browse', {
+      method: 'get'
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('server response:', data);//delete this
+        setBookList(data)
+      })
+      .catch(err => console.log("there was an error", err));
+  }, [libraryFilter]);
+
+  console.log("booklist state", bookList) //delete this
+
   return (
     <div id="library" className={classes.section}>
       <div className={classes.container}>
         <GridContainer justify="center">
-          {database.map(book => (
+          {bookList.map(book => (
             <GridItem xs={12} sm={12} md={6}>
               <LibraryCard
                 key={book.id}
@@ -32,8 +49,8 @@ export default function Library() {
                 author={book.author}
                 synopsis={book.synopsis}
                 reader={book.reader}
-                imageUrl={book.imageUrl}
-                coverUrl={book.coverUrl}
+                imageUrl={book.image_url}
+                coverUrl={book.cover_url}
               />
             </GridItem>
           ))}
