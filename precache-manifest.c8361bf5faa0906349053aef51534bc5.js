6a5b360ff982ede12ecc0c63a65ac1404f2640e5
steviesocks <stephen.stocking@gmail.com>
2020-06-29 16:22:19 -0400

    Updates

---
+++ precache-manifest.c8361bf5faa0906349053aef51534bc5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6738ec95a902ec48aa634fe79bb76ed",
+    "url": "/sproutish/index.html"
+  },
+  {
+    "revision": "2de06cd8e0dd731dd630",
+    "url": "/sproutish/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "8f2eb1b09cdb9f01b596",
+    "url": "/sproutish/static/js/2.11e4c554.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/sproutish/static/js/2.11e4c554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de06cd8e0dd731dd630",
+    "url": "/sproutish/static/js/main.b7d3a110.chunk.js"
+  },
+  {
+    "revision": "2b1e6888c4a932b88b82",
+    "url": "/sproutish/static/js/runtime-main.77c2f3ca.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/sproutish/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/sproutish/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/sproutish/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/sproutish/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "32ad5b43b2c3e2a8d49358b61fec4646",
+    "url": "/sproutish/static/media/landing-bg.32ad5b43.png"
+  },
+  {
+    "revision": "7b49f7fa4d1b49b7ad960945df476741",
+    "url": "/sproutish/static/media/sproutish-logo-black.7b49f7fa.png"
+  },
+  {
+    "revision": "6a390270c41507e032104002310d927c",
+    "url": "/sproutish/static/media/sproutish-logo-white.6a390270.png"
+  }
+]);
